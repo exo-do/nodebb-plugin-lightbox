44a47d7ea88f4e2533981cc18db3dccf22abfe62
Rubén Ballester <irbbau@gmail.com>
2015-01-18 19:45:04 +0100

    Removing console.log

---
+++ README.md
@@ -0,0 +1,26 @@
+# NodeBB LightBox Plugin
+
+Este plugin permite cargar las imágenes en un LightBox sin salir del foro
+
+
+Por un spoiler cuyo contenido es Esto es una prueba
+
+
+##Funcionamiento y compatibilidad
+
+El plugin está funcionando correctamente en la rama de NodeBB 0.6.X.
+
+
+La carpeta de imagenes hay que subirlas al directorio public
+
+
+## Instalación
+
+    Crea la carpeta "nodebb-plugin-lightbox", introduce los archivos e introduce la carpeta  dentro de "node_modules"
+    Ejecuta en la terminal "npm install"
+
+    La carpeta de imagenes hay que subirlas al directorio public
+
+## Problemas conocidos y características pendientes
+
+Ninguno
---
+++ library.js
@@ -0,0 +1,17 @@
+"use strict";
+
+var lightbox = {};
+
+  lightbox.lightboxPrettify = function (postContent, callback) {
+    postContent.postData.content = postContent.postData.content.replace( /<img src="?(.*)" alt="?(.+)" \/>/g , '<a class="lightboxlink" href="$1" data-lightbox="$2"><img class="lightboximage" src="$1" alt="$2"></a>');
+    /*postContent.postData.content = postContent.postData.content.replace( /<code<a class="lightboxlink" href="(.*)" data-lightbox="(.*)"><img class="?(.*)" src="?(.*)" alt="?(.*)"><\/a>/g , '<img class="$1" src="$2" alt="$3">');
+*/
+    callback(null, postContent);
+  };
+  lightbox.parseSignature = function (postContent, callback) {
+    postContent.userData.signature = postContent.userData.signature.replace(  /<img src="?(.*)" alt="?(.+)" \/>/g , '<a class="lightboxlink" href="$1" data-lightbox="$2"><img class="lightboximage" src="$1" alt="$2"></a>');
+    /*postContent.userData.signature = postContent.userData.signature.replace( /<a class="lightboxlink" href="(.*)" data-lightbox="(.*)"><img class="?(.*)" src="?(.*)" alt="?(.*)"><\/a>/g , '<img class="$1" src="$2" alt="$3">');
+    */
+    callback(null, postContent);
+  };
+module.exports = lightbox;
